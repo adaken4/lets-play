6e2b69277fb6d7d74d23b35284d4d0ad517d6e73
adaken4 <adakennedy@outlook.com>
2025-12-21 09:26:37 +0300

    chore: initial project scaffold from spring initializr

---
+++ src/test/java/com/github/adaken4/lets_play/LetsPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.adaken4.lets_play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
