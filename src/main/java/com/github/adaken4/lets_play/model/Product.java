6ada54942e0a5745483c72c35accef437c4e8db1
adaken4 <adakennedy@outlook.com>
2025-12-21 14:13:57 +0300

    feat(model): add Product entity with validation (#1)

---
+++ src/main/java/com/github/adaken4/lets_play/model/Product.java
@@ -0,0 +1,27 @@
+package com.github.adaken4.lets_play.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+@Data
+@Document(collection = "products")
+public class Product {
+    @Id
+    private String id;
+
+    @NotBlank
+    private String name;
+    private String description;
+
+    @NotNull
+    @PositiveOrZero
+    private Double price;
+
+    @NotBlank
+    private String userId;
+}
