6f34a793c6ddd350b601050daa4300740a4f8ccb
adaken4 <adakennedy@outlook.com>
2026-01-18 21:33:40 +0300

    feat(service): add validateUniqueEmail method (#4)

--- src/main/java/com/github/adaken4/lets_play/service/UserService.java
+++ src/main/java/com/github/adaken4/lets_play/service/UserService.java
@@ -1,14 +1,26 @@
 package com.github.adaken4.lets_play.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.adaken4.lets_play.dto.UserResponse;
 import com.github.adaken4.lets_play.model.User;
+import com.github.adaken4.lets_play.repository.UserRepository;
 
 @Service
 public class UserService {
 
-    
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * Single query email check prevents race conditions during concurrent registration
+     */
+    private void validateUniqueEmail(String email) {
+        if (userRepository.existsByEmail(email)) {
+            throw new RuntimeException("Email already in use");
+        }
+    }
 
     /**
      * Security: never expose password field in responses
