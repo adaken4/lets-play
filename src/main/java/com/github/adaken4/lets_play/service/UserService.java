ee3092477b3068e81575250668912c16f4c2092c
adaken4 <adakennedy@outlook.com>
2026-01-18 21:36:39 +0300

    feat(service): add a createUser method with unique email validation (#4)

--- src/main/java/com/github/adaken4/lets_play/service/UserService.java
+++ src/main/java/com/github/adaken4/lets_play/service/UserService.java
@@ -1,17 +1,39 @@
 package com.github.adaken4.lets_play.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.github.adaken4.lets_play.dto.UserCreationRequest;
 import com.github.adaken4.lets_play.dto.UserResponse;
 import com.github.adaken4.lets_play.model.User;
 import com.github.adaken4.lets_play.repository.UserRepository;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    /**
+     * Creates a user after email uniqueness check.
+     */
+    public UserResponse createUser(UserCreationRequest request) {
+        validateUniqueEmail(request.email());
+        User user = new User(
+            UUID.randomUUID().toString(),
+            request.name(),
+            request.email(),
+            encoder.encode(request.password()), // Always hash before save
+            request.role()
+        );
+        return mapToResponse(userRepository.save(user));
+    }
 
     /**
      * Single query email check prevents race conditions during concurrent registration
